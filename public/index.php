160c0327a38153bb1894a0ff576b0e4d1a130a79
Daniel Amarante <danz82158@gmail.com>
2018-11-25 19:30:07 -0200

    Iniciando Projeto

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+	<title>Teste Certificacao</title>
+	<link rel="stylesheet" href="app/css/header.css">
+</head>
+<body>
+<h1>Ola</h1>
+</body>
+</html>
