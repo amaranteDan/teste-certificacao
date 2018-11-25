727ecf4eccbda17a580a48301a4debdbf9f929a6
Daniel Chung <danielvchung@hotmail.com>
2018-11-25 20:27:29 -0200

    Criando cabeçalho da página

--- public/index.php
+++ public/index.php
@@ -4,10 +4,32 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale1">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge"> 
-	<title>Teste Certificacao</title>
+	<title>Teste Certificação</title>
+	<link rel="stylesheet" href="app/css/reset.css">
 	<link rel="stylesheet" href="app/css/header.css">
 </head>
 <body>
-<h1>Ola</h1>
+	<header class="header">
+		<nav class="main-menu">
+			<ul>
+				<li class="menu-item"><a href="#">Objetivo</a></li>
+				<li class="menu-item"><a href="#">Fundadores</a></li>
+				<li class="menu-item"><a href="#">Sobre</a></li>
+				<li class="menu-item"><a href="#">Contato</a></li>
+			</ul>
+		</nav>
+		<div class="jumbotron">
+			<section class="panel-main">
+				<h1 class="title">Teste Certificação</h1>
+				<h2 class="subtitle">Tecnologia</h2>
+			</section>
+			<section class="panel-test">
+				<a class="init-test" href="#">Iniciar Teste</a>
+			</section>
+		</div>
+	</header>
+	<section></section>
+	<section></section>
+	<section></section>
 </body>
 </html>
